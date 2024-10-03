b0e1047bc8da302b9ca0a0e17d926206cf955711
hsayadi4 <hamza.sayadi@epitech.digital>
2024-10-03 16:45:15 +0200

    Create ex_05.js

---
+++ ex_05.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+let addToCartButton = document.getElementById('addToCart');
+addToCartButton.addEventListener('click', function() {
+  alert('Thanks!');
+});
+});
